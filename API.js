8ecbed532b7d52d0fd277ef8141e4edad8213bb0
Same <110787671+Same-unix@users.noreply.github.com>
2023-05-05 18:43:51 -0500

    The API no key
    USE THIS AT YOUR OWN RISK

---
+++ API.js
@@ -0,0 +1,17 @@
+// you can just copy and paste the API
+// note replace the whatever with whatever you want :|
+function ActivateBrain() {
+    // What these scripts do is when submit is pressed then it will respond.
+    if (whatever = "hello") {
+        AIoutput.innerText = "Hello how are you"; // Sorry if im doing DRY
+    }
+    if (whatever == "Hello") {
+        AIoutput.innerText = "Hello how are you";
+    }
+    if (whatever === "Hi") {
+        AIoutput.innerText = "Hi how are you";
+    }
+    if (whatever === "Whats 2+2") {
+        document.write("2 + 2");
+    }
+}
